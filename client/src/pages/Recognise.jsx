a3620f9b826d618185f50dc1452dd339f2722437
HariKiran <harikiran.kante@gmail.com>
2023-02-22 04:21:17 +0530

    linked recognise component with backend

--- client/src/pages/Recognise.jsx
+++ client/src/pages/Recognise.jsx
@@ -1,13 +1,46 @@
+import { useState } from "react";
+import backend from "../backend";
 const Recognise = () => {
-  const handleSubmit = (e) => {
+  const [selectedImage, setSelectedImage] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
+    if (selectedImage === null) {
+      alert("No Image is selected.");
+      return;
+    }
+    e.target.disabled = true;
+    try {
+      const response = await backend.post(
+        "/recognise",
+        {
+          img: selectedImage,
+        },
+        {
+          headers: { "Content-Type": "multipart/form-data" },
+        }
+      );
+      console.log(response);
+      if (response.data.message === "User Found") {
+        setMessage(`Name: ${response.data.user.name}`);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+    e.target.disabled = false;
+  };
+
+  const handleChange = (e) => {
+    setSelectedImage(e.target.files[0]);
   };
   return (
     <div className="recognise">
       <form>
-        <input type="file" name="img" id="image" />
+        <input type="file" name="img" id="image" onChange={handleChange} />
         <input type="submit" value="Recognise" onClick={handleSubmit} />
       </form>
+      <div className="message">{message}</div>
     </div>
   );
 };
