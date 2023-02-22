1a433aeb71c084951dc45bb23c184422b655aee0
HariKiran <harikiran.kante@gmail.com>
2023-02-22 23:23:56 +0530

    change backend url

--- client/src/backend.js
+++ client/src/backend.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const backend = axios.create({
-  baseURL: "http://localhost:8000",
-  // baseURL: "https://face-recognition-backend-7jvfwimwla-uc.a.run.app",
+  // baseURL: "http://localhost:8000",
+  baseURL: "https://face-recognition-backend-7jvfwimwla-uc.a.run.app",
 });
 
 export default backend;
